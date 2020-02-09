f591968703b0e89dda02987473f7deb1310a6acb
SimonLi <chenmoonmo@gmail.com>
2020-02-09 19:58:26 +0800

    axios

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ server.post('/post',(req,res)=>{
     let user = req.body;
     res.send(`${user.uname},${user.time}好`)
 })
+server.get('/axios',(req,res)=>{
+    res.send('hello axios!')
+})
 server.get('/json',(req,res)=>{
     let data ={
         "uname": "simon",
