7be7f4cce535c117fe1485a4f9656a3e38cc3d2c
SimonLi <chenmoonmo@gmail.com>
2020-02-09 18:55:23 +0800

    fetch2

--- app.js
+++ app.js
@@ -7,4 +7,11 @@ server.post('/post',(req,res)=>{
     let user = req.body;
     res.send(`${user.uname},${user.time}好`)
 })
+server.get('/json',(req,res)=>{
+    let data ={
+        "uname": "simon",
+        "age" : 18
+    };
+    res.send(data);
+})
 server.listen(3000,console.log('server is running'));
